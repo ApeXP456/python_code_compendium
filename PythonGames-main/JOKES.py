c332aa57399a4e9b8732b98363382f81f0835980
ApeXP <91237308+ApeXP456@users.noreply.github.com>
2025-05-25 17:25:58 -0400

    Update JOKES.py

--- PythonGames-main/JOKES.py
+++ PythonGames-main/JOKES.py
@@ -14,3 +14,47 @@ print('Interupting cow.')
 input()
 print('Interupting cow wh', end='')
 print('-MOO!')
+
+print('Why don’t skeletons fight each other?')
+input()
+print('They don’t have the guts.')
+
+print('What did the fish say when it hit the wall?')
+input()
+print('Dam!')
+
+print('Why don’t scientists trust atoms?')
+input()
+print('Because they make up everything!')
+
+print('How does a penguin build its house?')
+input()
+print('Igloos it together!')
+
+print('Why did the scarecrow win an award?')
+input()
+print('Because he was outstanding in his field!')
+
+print('Knock, knock')
+input()
+print('Lettuce')
+input()
+print('Lettuce who?')
+input()
+print('Lettuce in, it’s cold out here!')
+
+print('Why did the math book look sad?')
+input()
+print('Because it had too many problems.')
+
+print('Why can’t you give Elsa a balloon?')
+input()
+print('Because she’ll let it go!')
+
+print('What’s orange and sounds like a parrot?')
+input()
+print('A carrot!')
+
+print('Why did the computer go to therapy?')
+input()
+print('It had too many bytes of emotional baggage.')
